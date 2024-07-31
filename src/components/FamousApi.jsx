8e97563efc865fae58d439656109d0c10bff9779
Man-joke <akstns90@gmail.com>
2024-07-31 13:55:02 +0900

    파일 구조명 변경

--- src/components/FamousApi.jsx
+++ src/components/FamousApi.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, CardBody, CardTitle } from "react-bootstrap";
 
-import "../css/FamousApi.scss";
+import "../scss/FamousApi.scss";
 
 const FamousApi = () => {
   return (
